9c10751062c3ecd06fb69647780b7206c9e85681
pavel <hidrexhidrex@gmail.com>
2022-07-20 17:33:43 +0300

    rerender

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-let rerender = (props) => {
+export let rerender = (props) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App rerender={rerender}/>
